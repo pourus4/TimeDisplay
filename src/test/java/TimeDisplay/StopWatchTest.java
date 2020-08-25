c5323b7621d3094864767e2539b66d89e0fdd7e4
pourus4 <55164633+pourus4@users.noreply.github.com>
2020-08-25 00:14:26 -0400

    add test class

---
+++ src/test/java/TimeDisplay/StopWatchTest.java
@@ -0,0 +1,16 @@
+package TimeDisplay;
+
+import javax.swing.JFrame;
+
+public class StopWatchTest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+
+        Stopwatch stopwatch = new Stopwatch("start/stop");
+        stopwatch.Start();
+        frame.add(stopwatch);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
